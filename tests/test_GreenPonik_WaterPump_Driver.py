b441b5f241b134688b42dcdf0d589a44a2808afa
MkLHX <mickael.lehoux@gmail.com>
2020-07-28 16:40:11 +0200

    fix missing method

--- tests/test_GreenPonik_WaterPump_Driver.py
+++ tests/test_GreenPonik_WaterPump_Driver.py
@@ -5,23 +5,37 @@
 import unittest
 
 
-from GreenPonik_WaterPump_Driver.GreenPonik_WaterPump_Driver import I2C_REGISTER, I2C_DEVICES_TYPE, i2c_scanner, read_byte_data
+from GreenPonik_WaterPump_Driver.GreenPonik_WaterPump_Driver import (
+    I2C_REGISTER,
+    I2C_DEVICES_TYPE,
+    i2c_scanner,
+    read_byte_data,
+    read_block_data,
+)
 
 
 class TestGreenPonik_WaterPump_Driver(unittest.TestCase):
     def test_i2c_scanner(self):
         devices = i2c_scanner()
         self.assertTrue(self, len(devices) > 0)
-        self.assertTrue(self, type(devices).__name__ == 'list')
-        
+        self.assertTrue(self, type(devices).__name__ == "list")
 
     def test_is_water_pump(self):
         for device in i2c_scanner():
-            self.assertTrue(self, I2C_DEVICES_TYPE['WATERPUMP'] == read_byte_data(device, I2C_REGISTER['TYPE']))
+            self.assertTrue(
+                self,
+                I2C_DEVICES_TYPE["WATERPUMP"]
+                == read_byte_data(device, I2C_REGISTER["TYPE"]),
+            )
 
     def test_have_uuid(self):
         for device in i2c_scanner():
-            self.assertTrue(self, type(read_block_data(device, I2C_REGISTER['UUID'])).__name__ == 'bytesarray' )
+            self.assertTrue(
+                self,
+                type(read_block_data(device, I2C_REGISTER["UUID"])).__name__
+                == "bytesarray",
+            )
+
 
 if __name__ == "__main__":
     unittest.main()
