eabbbb2ad340e33fb487513ebf718239e5424fa5
Mickael <mickael.lehoux@greenponik.com>
2020-09-16 11:28:05 +0200

    remove unusefull code

--- tests/test_WaterPumpDriver.py
+++ tests/test_WaterPumpDriver.py
@@ -13,63 +13,20 @@ class FcntlMock:
         pass
 
 
-class BoardMock:
-    def __init__(self):
-        self._scl = 18
-        self._sda = 15
-
-    def SCL(self):
-        return self._scl
-
-    def SDA(self):
-        return self._sda
-
-
-class BusioMock:
-    def I2C(self, sda, scl):
-        return True
-
-
 sys.modules["fcntl"] = FcntlMock()
-sys.modules["board"] = BoardMock()
-sys.modules["busio"] = BusioMock()
 
 
 class TestWaterPumpDriver(unittest.TestCase):
-    # @patch("GreenPonik_WaterPump_Driver.WaterPumpDriver")
-    # def test_i2c_scanner(self, mock):
-    #     d = mock()
-    #     expected = [i for i in range(20, 101)]
-    #     d.scan.return_value = expected
-    #     devices = d.scan()
-    #     self.assertIsNotNone(devices)
-    #     self.assertTrue(len(devices) > 0)
-    #     self.assertTrue(type(devices).__name__ == "list")
-
-    # @patch("GreenPonik_WaterPump_Driver.WaterPumpDriver")
-    # def test_read(self, mock):
-    #     d = mock()
-    #     d.read.return_value = WaterPumpDriver.I2C_DEVICES_TYPE["WATERPUMP"]
-    #     devices = WaterPumpDriver.i2c_scanner()
-    #     self.assertIsNotNone(devices)
-    #     deviceType = d.read(devices[0], WaterPumpDriver.I2C_REGISTERS["TYPE"])
-    #     self.assertIsNotNone(deviceType)
-    #     self.assertTrue(type(deviceType).__name__ == "int")
-    #     self.assertTrue(deviceType == WaterPumpDriver.I2C_DEVICES_TYPE["WATERPUMP"])
-
-    # @patch("GreenPonik_WaterPump_Driver.WaterPumpDriver")
-    # def test_read(self, mock):
-    #     d = mock()
-    #     d.read.return_value = [55, 89, 63, 35, 54, 21, 25, 47]
-    #     devices = WaterPumpDriver.i2c_scanner(self)
-    #     self.assertIsNotNone(self, devices)
-    #     b = bytearray(8)
-    #     UUID = (d.read(devices[0], WaterPumpDriver.I2C_REGISTERS["UUID"], b),)
-    #     self.assertIsNotNone(self, UUID)
-    #     self.assertTrue(
-    #         self, type(UUID).__name__ == "list",
-    #     )
-    #     self.assertTrue(self, len(UUID) == 8)
+    @patch("GreenPonik_WaterPump_Driver.WaterPumpDriver.WaterPumpDriver")
+    def test_get_uuid(self, mock):
+        d = mock()
+        expected = b"\x55\x89\x63\x35\x54\x21\x25\x47"
+        d.get_uuid.return_value = expected
+        uuid = d.get_uuid()
+        self.assertIsNotNone(uuid)
+        self.assertTrue(type(uuid).__name__ == "bytes")
+        self.assertTrue(len(uuid) == 8)
+        self.assertEqual(expected, uuid)
 
     @patch("GreenPonik_WaterPump_Driver.WaterPumpDriver.WaterPumpDriver")
     def test_on_off_pump(self, mock):
