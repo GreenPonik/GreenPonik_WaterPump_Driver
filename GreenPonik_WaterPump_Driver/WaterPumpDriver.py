a82d8dd692f6ec8282bb51b9c51a43c3dbacb05e
Mickael <mickael.lehoux@greenponik.com>
2020-09-28 13:08:41 +0200

    ok

--- GreenPonik_WaterPump_Driver/WaterPumpDriver.py
+++ GreenPonik_WaterPump_Driver/WaterPumpDriver.py
@@ -101,7 +101,6 @@ class WaterPumpDriver:
             with Unpacker() as unpacker:
                 unpacker.write(raw)
                 device_type = unpacker.read()[0]  # type data is the first field of list
-            print("device type is: ", device_type)
             if device_type == self.I2C_DEVICES_TYPE:
                 return True
             else:
