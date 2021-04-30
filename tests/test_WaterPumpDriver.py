5065ccb73b1d4638d55abb61af6f47c490e159e7
Mickael <mickael.lehoux@greenponik.com>
2021-04-30 10:42:12 +0200

    add test

--- tests/test_WaterPumpDriver.py
+++ tests/test_WaterPumpDriver.py
@@ -6,6 +6,20 @@ import sys
 import unittest
 from unittest.mock import patch
 
+I2C_REGISTERS = {
+    "TYPE": 0x00,  # i2c Read Only
+    "FIRMWARE": 0x01,  # i2c Read Only
+    "UUID": 0x02,  # i2c Read Only
+    "I2C_ADDRESS": 0x03,  # i2c Read / Write
+    "LED_ACTIVATION": 0x04,  # i2c Read / Write
+    "WATER_PUMP_STATE": 0x05,  # i2c Read / Write
+    "PUMP_1_STATE": 0x06,  # i2c Read / Write
+    "PUMP_2_STATE": 0x07,  # i2c Read / Write
+    "PUMP_3_STATE": 0x08,  # i2c Read / Write
+    "PUMP_4_STATE": 0x09,  # i2c Read / Write
+    "ALL": 0x20,  # i2c Read
+}
+
 
 class FcntlMock:
     def ioctl(self):
@@ -17,6 +31,18 @@ sys.modules["fcntl"] = FcntlMock()
 
 
 class TestWaterPumpDriver(unittest.TestCase):
+    @patch("GreenPonik_WaterPump_Driver.WaterPumpDriver.WaterPumpDriver")
+    def test_get_all(self, mock):
+        d = mock()
+        registers = I2C_REGISTERS
+        registers.pop("ALL", 0x20)
+        expected = [r for k, r in I2C_REGISTERS.items()]
+        d.get_all.return_value = expected
+        device_get_all = d.get_all()
+        self.assertIsNotNone(device_get_all)
+        self.assertTrue(type(device_get_all) is list)
+        self.assertEqual(expected, device_get_all)
+
     @patch("GreenPonik_WaterPump_Driver.WaterPumpDriver.WaterPumpDriver")
     def test_get_type(self, mock):
         d = mock()
@@ -31,7 +57,7 @@ class TestWaterPumpDriver(unittest.TestCase):
     def test_get_uuid(self, mock):
         d = mock()
         list_from_i2c = [0, 0, 204, 80, 227, 181, 155, 20]
-        expected = ''.join(map(str, list_from_i2c))
+        expected = "".join(map(str, list_from_i2c))
         d.get_uuid.return_value = expected
         uuid = d.get_uuid()
         self.assertIsNotNone(uuid)
