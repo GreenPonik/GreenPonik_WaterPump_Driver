c90b04877f2e8cc0ecd2859d64878694fcbc2c01
MkLHX <mickael.lehoux@greenponik.com>
2022-05-02 14:17:10 +0200

    bump to v0.0.15

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import os
 import pathlib
 from setuptools import setup, find_packages
 
@@ -9,7 +8,6 @@ URL = "https://github.com/GreenPonik/GreenPonik_WaterPump_Driver"
 EMAIL = "contact@greenponik.com"
 AUTHOR = "GreenPonik SAS"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.0.11"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -29,16 +27,11 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 # Load the package's version.py module as a dictionary.
-about = {}
-if not VERSION:
-    with open(os.path.join(here, "version.py")) as f:
-        exec(f.read(), about)
-else:
-    about["__version__"] = VERSION
+from version import __version__
 
 setup(
     name=NAME,
-    version=about["__version__"],
+    version=__version__,
     author=AUTHOR,
     author_email=EMAIL,
     description=DESCRIPTION,
